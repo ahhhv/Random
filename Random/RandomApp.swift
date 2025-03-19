80215a4bc4ce24b0eee58f5d3d87eeed89176146
Alex Hernández <ahernandezv85@gmail.com>
2025-03-19 21:40:28 +0100

    Initial Commit

---
+++ Random/RandomApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomApp.swift
+//  Random
+//
+//  Created by Alex Hernández on 19/3/25.
+//
+
+import SwiftUI
+
+@main
+struct RandomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
